1d451d0801c44bc93f85df659998ab05c04d30fc
Trevor Hu <trevorhu@Trevors-MacBook-Pro.local>
2020-10-24 09:04:38 -0400

    Function with documentation

---
+++ FunctionScript.R
@@ -0,0 +1,13 @@
+#Function script 
+#This function aims to simply take the square root of any given number
+#The function can receive an input (numeric) and will simply print out its square root
+
+SquareRoot<-function(x){
+  y<-sqrt(x)
+  
+  print(y)
+  
+}
+
+#test with:
+SquareRoot(4)
